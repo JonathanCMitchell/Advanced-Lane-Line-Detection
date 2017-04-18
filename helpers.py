2d10ee559960ec34188e2b05b0775e122f5f9422
jonathancmitchell <jmitchell1991@gmail.com>
2017-04-18 14:03:24 -0700

    [feature] - changes threshold paramters inside findWindowCentroids

--- helpers.py
+++ helpers.py
@@ -1,12 +1,12 @@
 
 def add_recent_centers(num, lst, arrayToTakeCentersFrom, smooth):
 	if bool(len(arrayToTakeCentersFrom)):
-		print('we are inside add_recent_centers arrayToTakeCentersFrom not none and is: ', arrayToTakeCentersFrom)
+		# print('we are inside add_recent_centers arrayToTakeCentersFrom not none and is: ', arrayToTakeCentersFrom)
 		# add centers from this array
 		i = 0
 		while num > len(lst):
 			if len(arrayToTakeCentersFrom) > smooth:
-				lst.append(arrayToTakeCentersFrom[:-smooth][i])	
+				lst.append(arrayToTakeCentersFrom[-smooth:][i])	
 			else:
 				lst.append(arrayToTakeCentersFrom[i])
 			i += 1

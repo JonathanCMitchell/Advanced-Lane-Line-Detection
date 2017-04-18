215021fa9950537d08860537b77bb5cdcf25ee84
jonathancmitchell <jmitchell1991@gmail.com>
2017-04-18 13:29:43 -0700

    [feature] - creates add_recent_centers helper function and changes output of window centroids from lists to tuples

---
+++ helpers.py
@@ -0,0 +1,21 @@
+
+def add_recent_centers(num, lst, arrayToTakeCentersFrom, smooth):
+	if bool(len(arrayToTakeCentersFrom)):
+		print('we are inside add_recent_centers arrayToTakeCentersFrom not none and is: ', arrayToTakeCentersFrom)
+		# add centers from this array
+		i = 0
+		while num > len(lst):
+			if len(arrayToTakeCentersFrom) > smooth:
+				lst.append(arrayToTakeCentersFrom[:-smooth][i])	
+			else:
+				lst.append(arrayToTakeCentersFrom[i])
+			i += 1
+		return lst
+	else:
+		for i in reversed(range(len(lst))):
+			if len(lst) >= num:
+				return lst
+			else:
+				lst.append(lst[i])
+				print('lst is: ', lst)
+		return lst
